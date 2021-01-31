9c1c97be6b0a0b4563a32879ef26d7c954e5e0a7
Richmond Chang <changrichmond@gmail.com>
2021-01-30 20:07:53 -0800

    [Lesson 122] - Add splash screen that loads into start menu

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private const string SPLASH_SCREEN = "01-splash-screen";
+    private const string START_MENU = "02-start-menu";
+
+    [SerializeField]
+    private float splashScreenLoadTime = 1.75f;
+
+    private void Start() {
+        string currScene = SceneManager.GetActiveScene().name;
+        Debug.Log(currScene);
+        switch (currScene) {
+            case SPLASH_SCREEN:
+                StartCoroutine(delayedLoad(START_MENU, splashScreenLoadTime));
+                break;
+        }
+    }
+
+    private IEnumerator delayedLoad(string scene, float delaySeconds) {
+        yield return new WaitForSeconds(delaySeconds);
+        SceneManager.LoadScene(scene);
+    }
+}
