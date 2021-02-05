8554ff0aeb3cd03512a90b95499a1ad7551148e3
Richmond Chang <changrichmond@gmail.com>
2021-02-04 22:44:36 -0800

    [Lesson 131] - Make lizard move to the left with script that translates transform

---
+++ Assets/Scripts/Walker.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Walker : MonoBehaviour
+{
+    [Range(-5f, 5f)]
+    [SerializeField]
+    private float walkSpeed = 1f;
+
+    private void Update() {
+        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+    }
+}
