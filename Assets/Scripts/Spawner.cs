14e6014a7fe17caecb6e7f651decae5263375f40
Richmond Chang <changrichmond@gmail.com>
2021-02-04 23:10:47 -0800

    [Lesson 133] - Add spawner for lizard:     -  Make objects into prefabs     -  Write script to spawn enemies on a random interval\     -  Add spawning script to game object and have one for each row of the garden

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemy;
+
+    [SerializeField]
+    private float minSpawnTime = 2f;
+
+    [SerializeField]
+    private float maxSpawnTime = 4f;
+
+    private bool isSpawning = true;
+
+    private IEnumerator Start() {
+        while (isSpawning) {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+            Instantiate(enemy, transform.position, Quaternion.identity);
+        }
+    }
+}
