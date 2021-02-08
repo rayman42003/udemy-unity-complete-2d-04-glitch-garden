b459368ce94d664c4b47433889b25c2bea75942f
Richmond Chang <changrichmond@gmail.com>
2021-02-07 19:35:06 -0800

    [Lesson 136] - Make projectile move:     -  Add Projectile script and attach to zucchini     -  BONUS: Added rotation, but needed to change translation so that it was with respect to the world rather than the object itself

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    private void Update() {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward * 120 * Time.deltaTime);
+    }
+}
