b8c8a74277c843ad9ef160a363c03b8140d48db5
Richmond Chang <changrichmond@gmail.com>
2021-02-07 19:04:04 -0800

    [Lesson 135] - Make cactus defender shoot:     -  Add animation event to cactus attack animation     -  Add projectile zucchini     -  Add script to allow cactus to shoot, add gameobject as child of cactus to use as position to shoot projectiles from

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject projectile;
+
+    [SerializeField]
+    private GameObject gun;
+
+    public void shoot() {
+        Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+    }
+}
