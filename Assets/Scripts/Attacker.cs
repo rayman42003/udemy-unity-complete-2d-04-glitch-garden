e5fe17f5259b66e8e220a18dfa9aef0e85d9ac28
Richmond Chang <changrichmond@gmail.com>
2021-02-08 23:53:01 -0800

    [Lesson 137] - Make Zucchinis do damage and make Lizards take damage:     -  Add attacker and damagable scripts     -  Add rigid bodies + colliders to Lizard and Zucchini         -  Note: Since we have both Zucchini and Lizard as Kinematic and not triggers, Lizard needs to have "Use Full Kinematic Contacts" setting set to allow onCollision events (perhaps Zucchini can just be a trigger)

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [SerializeField]
+    private int attackPower = 1;
+
+    [SerializeField]
+    private bool destroyOnCollision = true;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Damagable damagable = collision.gameObject.GetComponent<Damagable>();
+        if (damagable) {
+            damagable.takeDamage(attackPower);
+            if (destroyOnCollision) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Damagable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Damagable : MonoBehaviour
+{
+    [SerializeField]
+    private int hitPoints = 1;
+
+    private UnityEvent onDamaged = new UnityEvent();
+    private UnityEvent onKilled = new UnityEvent();
+
+    public void takeDamage(int amount) {
+        hitPoints -= amount;
+        if (hitPoints <= 0) {
+            onKilled.Invoke();
+            Destroy(gameObject);
+        }
+        onDamaged.Invoke();
+    }
+}
