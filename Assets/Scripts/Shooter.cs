e97e58d88a8d4ff9d4d9543eb6cf1eef4d70f5df
Richmond Chang <changrichmond@gmail.com>
2021-02-17 23:13:57 -0800

    [Lesson 152] - Defenders check for enemies in lane:     -  Adjust spawn location to line up with the grid system and adjust sprite/hitbox accordingly     -  Associate defender with the lane's spawner     -  Since enemies are children of the spawner, check if there are any children on the spawner

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,7 +8,30 @@ public class Shooter : MonoBehaviour
     [SerializeField]
     private GameObject gun;
 
+    [SerializeField]
+    private bool hasEnemiesInLane;
+
+    private EnemySpawner laneEnemySpawner;
+
+    private void Start() {
+        laneEnemySpawner = findLaneEnemySpawner();
+    }
+
+    private void Update() {
+        hasEnemiesInLane = laneEnemySpawner.transform.childCount > 0;
+    }
+
     public void shoot() {
         Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+    }
+
+    private EnemySpawner findLaneEnemySpawner() {
+        foreach (EnemySpawner spawner in FindObjectsOfType<EnemySpawner>()) {
+            float yDelta = this.gameObject.transform.position.y - spawner.transform.position.y;
+            if (Mathf.Abs(yDelta) <= Mathf.Epsilon) {
+                return spawner;
+            }
+        }
+        return null;
     }
 }
